95f6eeae30e2b543ad8d37efdc336be80a0c3dd0
sk8erpunk <lori.ramal@gmail.com>
2018-10-24 16:35:41 +0300

    1.1 solution

--- 1.1isUnique.cpp
+++ 1.1isUnique.cpp
@@ -1,9 +1,6 @@
 /******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
 *******************************************************************************/
 
 #include <iostream>
