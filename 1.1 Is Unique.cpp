280137fc4daff28fa2be4aee45f8c18bed8725c6
sk8erpunk <lori.ramal@gmail.com>
2019-01-12 14:23:40 +0200

    Updating existing file

--- 1.1 Is Unique.cpp
+++ 1.1 Is Unique.cpp
@@ -5,31 +5,30 @@ What if you cannot use additional data structures?
 
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
+
 #define MAX_CHAR 256
 
-
-bool isUnique(string str){
-    int chars[MAX_CHAR] = { 0 };
+// Time complexity is O(N) where N is the length of input
+// Memory space is O(1)
+bool isUnique(string& str){
+    vector<int> chars(MAX_CHAR,0);	// init to zeroes
     for(int i = 0; i < str.length(); i++){
-        if(++chars[str[i]] > 1){
+        if(++chars[(int)str[i]] > 1){
             return false;
         }
     }
-    
     return true;
 }
-
 
 int main()
 {
     string str = "hello world";
-    if(isUnique(str)){
+    if(isUnique(str))
         cout << "String is unique!";
-    } else {
+    else 
         cout << "String is not unique!";
-    }
     
     return 0;
 }
